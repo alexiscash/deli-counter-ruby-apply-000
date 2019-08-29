65891be3d7b3c053fc8846e6011ccb36df71f335
fortunate-commit-9523 <alexiscash97@gmail.com>
2019-08-29 20:13:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,21 @@
 # Write your code here.
 
+$currently_serving = 0
+
 def line(array)
   if array == []
     puts "The line is currently empty."
   else
     line_order = ""
     array.each_with_index { |i,j| line_order += "#{j+1}. #{i} " }
-    line_order.chop!
-    puts "The line is currently: " + line_order
+    puts "The line is currently: " + line_order.chop!
   end
 end
 
-def take_a_number(array, name)
-  array.push(name)
-  puts "Welcome, #{name}. You are number #{array.length} in line."
+def take_a_number(array)
+  $currently_serving += 1
+  array.push($currently_serving)
+  puts "Welcome, you are number #{$currently_serving}."
 end
 
 def now_serving(array)
